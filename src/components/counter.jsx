942dfdc901263fb11c62fbf524fb9e105b03b415
Haruto Sato <k097dynasty@icloud.com>
2019-10-21 21:40:32 +0900

    initial commit

---
+++ src/components/counter.jsx
@@ -0,0 +1,47 @@
+import React, {Component} from 'react';
+
+
+class Counter extends Component {
+    componentDidUpdate(prevProps, prevState) {
+        console.log('prevProps', prevProps);
+        console.log('prevState', prevState);
+        if(prevProps.counter.value !== this.props.counter.value) {
+            //ajax called and get new data from server
+
+      }
+    }
+   
+componentWillUnmount() {
+    console.log('Counter - Unmount');
+}
+
+//react is comparing the virtual dom with the old one and figure out what is changed or what has been changed as a result
+
+    render() {
+        console.log('Counter - Render');    
+        return (
+           
+        <div>
+                <span className ={this.getBadgeClasses()}>{this.formatCount()}</span>
+                <button onClick={()=> this.props.onIncrement(this.props.counter)} className="btn btn-secondary btn-sm">Increment</button>
+                <button onClick={ ()=> this.props.onDelete(this.props.counter.id)} className="btn btn-danger btn-sm m-2">Delete</button>
+            </div>
+        );
+    }
+
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += this.props.counter.value === 0 ? "warning": "primary";
+        return classes;
+    }
+
+    
+ 
+    formatCount() {
+        const  value  = this.props.counter.value;
+        return value === 0 ? <h2>Zero</h2> : value;
+      
+    }
+
+}
+export default Counter;
---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import "index.css";
+import App from "./App";
+import 'bootstrap/dist/css/bootstrap.css';
+
+
+const element = <h1>HEllo World</h1>;
+
+ReactDOM.render(<App/>, document.getElementById('root'));
+
